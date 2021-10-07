fee2a187fce0d4de344a1665224e6202659ca116
sanjay <sanjayjaiswal586@gmail.com>
2021-10-07 08:28:54 +0530

    Making news component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 
 import React, { Component } from 'react'
 import NavBar from './component/NavBar'
+import News from './component/News'
 
 export default class App extends Component {
   render() {
     return (
       <div>
 
-        <NavBar></NavBar>
+        <NavBar/>
+        <News/>
+
         
       </div>
     )
---
+++ src/component/News.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export default class News extends Component {
+    render() {
+        return (
+            <div>
+                This is news component
+                <NewsItem/>
+                <NewsItem/>
+
+                <NewsItem/>
+
+                <NewsItem/>
+
+
+                
+            </div>
+        )
+    }
+}
