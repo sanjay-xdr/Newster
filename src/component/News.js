5c681033657a687cc616cbc608a4974b5f517ba5
sanjay <sanjayjaiswal586@gmail.com>
2021-10-07 17:10:28 +0530

    displaying news

--- src/component/News.js
+++ src/component/News.js
@@ -2,42 +2,51 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export default class News extends Component {
+
+
+
+    constructor(){
+        super();
+        this.state={
+            articles:[],
+            loading:false
+        }
+    }
+
+    async componentDidMount(){
+        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=8508c10eba8b4e8e93fb8978ecbba7f8&page=1";
+        let data=await fetch(url);
+        let parsedData=await data.json();
+        this.setState({articles: parsedData.articles})
+
+    }
+
+
+
+
     render() {
         return (
             <div className="container my-3">
                 <h1>this is newster</h1>
                 This is news component
                 <div className="row my-3 mx-4">
+
+                    {this.state.articles.map((element,id)=>{
+                           return <div key={id} className="col md-4 my-4">
+                           <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+                       </div>
+                    })}
+
+                    <div className="container d-flex justify-content-between">
+                    <button type="button" class="btn btn-dark">Previous</button>
+                    <button type="button" class="btn btn-dark">Next</button>
+
+                    </div>
                     
-                    <div className="col md-4 my-4">
-                        <NewsItem title="This is title" description="i am the description" />
-                    </div>
+                 
 
-                    <div className="col md-4 my-4">
-                        <NewsItem title="This is title" description="i am the description" />
-                    </div>
-
-                    <div className="col md-4 my-4">
-                        <NewsItem title="This is title" description="i am the description" />
-                    </div>
-
-                    <div className="col md-4 my-4">
-                        <NewsItem title="This is title" description="i am the description" />
-                    </div>
-
-                    <div className="col md-4 my-4">
-                        <NewsItem title="This is title" description="i am the description" />
-                    </div>
-                   
-                    <div className="col md-4 my-4">
-                        <NewsItem title="This is title" description="i am the description" />
-                    </div>
-                    <div className="col md-4 my-4">
-                        <NewsItem title="This is title" description="i am the description" />
-                    </div>
-                    <div className="col md-4 my-4">
-                        <NewsItem title="This is title" description="i am the description" />
-                    </div>
+               
+                 
               
                 </div>
 
--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
+
+ 
     render() {
-        let {title,description}=this.props;
+        let {title,description,imageUrl,newsUrl}=this.props;
         return (
             <div>
 
                 <div className="card" style={{ width:"18rem"}}>
-                <img src="https://cbsnews1.cbsistatic.com/hub/i/r/2021/10/07/a7de9569-670e-4abc-a7a2-0d52dbdf58a0/thumbnail/1200x630/5d354c2820d8e17c4738f62f54c8a83a/1006-en-full-809472-640x360.jpg" className="card-img-top" alt="..."/>
+                <img src={imageUrl?imageUrl:"https://images.pexels.com/photos/936137/pexels-photo-936137.jpeg?cs=srgb&dl=pexels-nappy-936137.jpg&fm=jpg"} className="card-img-top" alt=" Business News Image " style={{height:"250px",width:"250px"}}/>
                 <div className="card-body">
                     <h5 className="card-title">{title}</h5>
                     <p className="card-text">{description}</p>
-                    <a href="#" className="btn btn-primary">Read More</a>
+                    <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
                 </div>
                 </div>
                
