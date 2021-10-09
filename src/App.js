45eee8376c3d6c55df7225fb312bd6864e18b172
sanjay <sanjayjaiswal586@gmail.com>
2021-10-09 13:21:33 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -18,14 +18,14 @@ export default class App extends Component {
         <NavBar/>
         
         <Switch>
-          <Route exact path="/"> <News pageSize={10} key="general" country="in" category="general"/> </Route>
-          <Route exact path="/business"> <News key="business" pageSize={10} country="in" category="business"/> </Route>
-          <Route exact path="/entertainment"> <News key="entertainment" pageSize={10} country="in" category="entertainment"/> </Route>
-          <Route exact path="/general"> <News key="general" pageSize={10} country="in" category="general"/> </Route>
-          <Route exact path="/health"> <News key="health" pageSize={10} country="in" category="health"/> </Route>
-          <Route exact path="/science"> <News key="science" pageSize={10} country="in" category="science"/> </Route>
-          <Route exact path="/sports"> <News key="sports" pageSize={10} country="in" category="sports"/> </Route>
-          <Route exact path="/technology"> <News key="technology" pageSize={10} country="in" category="technology"/> </Route>
+          <Route exact path="/"> <News pageSize={12} key="general" country="in" category="general"/> </Route>
+          <Route exact path="/business"> <News key="business" pageSize={12} country="in" category="business"/> </Route>
+          <Route exact path="/entertainment"> <News key="entertainment" pageSize={12} country="in" category="entertainment"/> </Route>
+          <Route exact path="/general"> <News key="general" pageSize={12} country="in" category="general"/> </Route>
+          <Route exact path="/health"> <News key="health" pageSize={12} country="in" category="health"/> </Route>
+          <Route exact path="/science"> <News key="science" pageSize={12} country="in" category="science"/> </Route>
+          <Route exact path="/sports"> <News key="sports" pageSize={12} country="in" category="sports"/> </Route>
+          <Route exact path="/technology"> <News key="technology" pageSize={12} country="in" category="technology"/> </Route>
 
 
         
